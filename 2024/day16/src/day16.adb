4bfe57e87c9c25c29c4661a835ea52ebecfb7564
Derek Kingston <derek.kingston@gmail.com>
2024-12-21 16:14:02 -0500

    AoC 2024 day 16 part 1 with full depth first search

--- 2024/day16/src/day16.adb
+++ 2024/day16/src/day16.adb
@@ -1,4 +1,163 @@
-procedure Day16 is
+with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Strings.Fixed; use Ada.Strings.Fixed;
+
+procedure day16 with SPARK_Mode is
+   PUZZLE_SIZE : constant Natural := 142;
+   type String_List is array (Positive range <>) of String (1 .. PUZZLE_SIZE);
+   type Direction is (North, East, South, West);
+   type Integer_Array is array (Direction) of Integer;
+   type Integer_Matrix is array (Positive range <>) of Integer_Array;
+   type Integer_Matrix3 is array (Positive range <>)
+      of Integer_Matrix (1 .. PUZZLE_SIZE);
+   type Pose is record
+      Row : Integer;
+      Col : Integer;
+      Dir : Direction;
+   end record;
+
+   File_Name    : constant String := "input.txt";
+   File         : File_Type;
+   Line         : String (1 .. PUZZLE_SIZE);
+   Line_Last    : Natural;
+   Puzzle       : String_List (1 .. PUZZLE_SIZE);
+   Cost_Map     : Integer_Matrix3 (1 .. PUZZLE_SIZE);
+   Row_Len      : Natural := 0;
+   Col_Len      : Natural := 0;
+   Start_Idx    : Natural := 0;
+   Reindeer     : Pose;
+   Min_Cost     : Natural := Natural'Last;
+
+   procedure Clear_Cost_Map is
+   begin
+      for I in 1 .. Row_Len loop
+         for J in 1 .. Col_Len loop
+            for D in Direction loop
+               Cost_Map (I) (J) (D) := Natural'Last;
+            end loop;
+         end loop;
+      end loop;
+   end Clear_Cost_Map;
+
+   --  should probably just use Dijkstra; full depth first search instead
+   procedure FindMinPath (P : Pose; Cost : Natural) is
+      R : Pose;
+   begin
+      --  Put_Line ("At (" & P.Row'Image & ", " & P.Col'Image
+      --     & ", " & P.Dir'Image & ")");
+      --  base case: already has lower cost
+      if Cost_Map (P.Row) (P.Col) (P.Dir) < Cost then
+         return;
+      end if;
+
+      --  base case: reached end, update min cost
+      if Puzzle (P.Row) (P.Col) = 'E' then
+         --  Put_Line ("### FOUND IT ###");
+         if Cost < Min_Cost then
+            Min_Cost := Cost;
+         end if;
+         return;
+      end if;
+
+      --  base case: hit obstacle
+      if Puzzle (P.Row) (P.Col) = '#' then
+         return;
+      end if;
+
+      --  mark cost along this path
+      Cost_Map (P.Row) (P.Col) (P.Dir) := Cost;
+
+      --  recurse each direction (North)
+      R := P;
+      R.Row := R.Row - 1;
+      R.Dir := North;
+      case P.Dir is
+         when North =>
+            FindMinPath (R, Cost + 1);
+         when East =>
+            FindMinPath (R, Cost + 1001);
+         when South =>
+            null;  --  don't go backwards
+         when West =>
+            FindMinPath (R, Cost + 1001);
+      end case;
+
+      --  recurse each direction (East)
+      R := P;
+      R.Col := R.Col + 1;
+      R.Dir := East;
+      case P.Dir is
+         when North =>
+            FindMinPath (R, Cost + 1001);
+         when East =>
+            FindMinPath (R, Cost + 1);
+         when South =>
+            FindMinPath (R, Cost + 1001);
+         when West =>
+            null;  --  don't go backwards
+      end case;
+
+      --  recurse each direction (South)
+      R := P;
+      R.Row := R.Row + 1;
+      R.Dir := South;
+      case P.Dir is
+         when North =>
+            null;  --  don't go backwards
+         when East =>
+            FindMinPath (R, Cost + 1001);
+         when South =>
+            FindMinPath (R, Cost + 1);
+         when West =>
+            FindMinPath (R, Cost + 1001);
+      end case;
+
+      --  recurse each direction (West)
+      R := P;
+      R.Col := R.Col - 1;
+      R.Dir := West;
+      case P.Dir is
+         when North =>
+            FindMinPath (R, Cost + 1001);
+         when East =>
+            null;  --  don't go backwards
+         when South =>
+            FindMinPath (R, Cost + 1001);
+         when West =>
+            FindMinPath (R, Cost + 1);
+      end case;
+   end FindMinPath;
+
 begin
-   null;
-end Day16;
+   --  Open the file for reading
+   Open (File => File, Mode => In_File, Name => File_Name);
+
+   --  Read the file line by line
+   while not End_Of_File (File) and then Row_Len < Puzzle'Length loop
+      Get_Line (File, Line, Line_Last);
+      if Col_Len = 0 then
+         Col_Len := Line_Last;
+      end if;
+      if Col_Len /= Line_Last then
+         Put_Line ("All rows must be of the same length");
+         exit;
+      end if;
+      Row_Len := Row_Len + 1;
+      Puzzle (Row_Len) := Line;
+
+      Start_Idx := Index (Source => Line (1 .. Line_Last),
+         Pattern => "S", From => 1);
+      if Start_Idx /= 0 then
+         Reindeer.Row := Row_Len;
+         Reindeer.Col := Start_Idx;
+         Reindeer.Dir := East;
+      end if;
+   end loop;
+
+   --  Close the file
+   Close (File);
+
+   Clear_Cost_Map;
+   FindMinPath (Reindeer, 0);
+   Put_Line ("Part A: " & Min_Cost'Image);
+
+end day16;
