f0237fde07442ff1ec2f1f3ddca9cc3459ad0e75
Derek Kingston <derek.kingston@gmail.com>
2025-12-06 11:06:19 -0500

    Set proper compiler flags in alire.toml

--- 2025/tryspark/src/tryspark.adb
+++ 2025/tryspark/src/tryspark.adb
@@ -25,9 +25,9 @@ procedure TrySpark with
          Put_Line ("Cannot write value!");
    end Put_Val;
 
-   P1 : constant InputData.Point := (X => 0, Y => 10);
-   P2 : constant InputData.Point := (X => 5, Y => 0);
-   Rt : constant InputData.Rectangle := (Top_Left => P1, Bottom_Right => P2);
+   P1 : constant Shapes.Point := (X => 0, Y => 10);
+   P2 : constant Shapes.Point := (X => 5, Y => 0);
+   Rt : constant Shapes.Rectangle := (Top_Left => P1, Bottom_Right => P2);
 begin
    ExpectOne : Integer := 0;
    Increment (ExpectOne);
@@ -35,6 +35,5 @@ begin
    Put_Line ("Incrementing safely!");
    Put_Val (ExpectOne);
    Put_Line ("");
-   Put_Line ("Rectangle Area: " &
-      Shapes.UInt64'Image (InputData.Area (Rt)));
+   Put_Line ("Rectangle Area: " & Shapes.UInt64'Image (Shapes.Area (Rt)));
 end TrySpark;
