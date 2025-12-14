a35c76df2d411513f44814f8c981fddd71f0441f
Derek Kingston <derek.kingston@gmail.com>
2025-12-14 10:49:31 -0500

    Day 09 of 2025 complete, both parts

--- 2025/day09/main.cpp
+++ 2025/day09/main.cpp
@@ -1,41 +1,33 @@
-#include <cmath>
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <map>
-#include <set>
 #include <vector>
 #include <sstream>
 #include <algorithm>
 
-class Coords {
+class Point {
 public:
     int64_t x;
     int64_t y;
 
-    // Define ordering for std::set
-    bool operator<(const Coords& other) const {
+    // Define lexicographical ordering
+    bool operator<(const Point& other) const {
         if (x != other.x) return x < other.x;
         return y < other.y;
     }
 };
 
-class Rectangle {
+class Segment {
 public:
-    Coords a;
-    Coords b;
-    int64_t area;
-    bool isInside(const Coords& c) const {
-        return c.x > std::min(a.x, b.x) && c.x < std::max(a.x, b.x) &&
-               c.y > std::min(a.y, b.y) && c.y < std::max(a.y, b.y);
-    }
+    Point start;
+    Point end;
 };
 
-bool crosses(const std::pair<Coords, Coords>& p, const std::pair<Coords, Coords>& q) {
-    int64_t p_deltaX = p.second.x - p.first.x;
-    int64_t p_deltaY = p.second.y - p.first.y;
-    int64_t q_deltaX = q.second.x - q.first.x;
-    int64_t q_deltaY = q.second.y - q.first.y;
+bool intersects(const Segment& p, const Segment& q) {
+    int64_t p_deltaX = p.end.x - p.start.x;
+    int64_t p_deltaY = p.end.y - p.start.y;
+    int64_t q_deltaX = q.end.x - q.start.x;
+    int64_t q_deltaY = q.end.y - q.start.y;
     if(p_deltaX == 0 && q_deltaX == 0) {
         // both vertical
         return false;
@@ -46,15 +38,43 @@ bool crosses(const std::pair<Coords, Coords>& p, const std::pair<Coords, Coords>
     }
     if(p_deltaX == 0 && q_deltaY == 0) {
         // p vertical, q horizontal
-        return (q.first.x <= p.first.x && p.first.x <= q.second.x) &&
-               (p.first.y <= q.first.y && q.first.y <= p.second.y);
+        int64_t x = p.start.x;
+        int64_t y = q.start.y;
+        return (std::min(q.start.x,q.end.x) < x && std::max(q.start.x,q.end.x) > x) && 
+               (std::min(p.start.y,p.end.y) < y && std::max(p.start.y,p.end.y) > y);
     }
     if(p_deltaY == 0 && q_deltaX == 0) {
         // p horizontal, q vertical
-        return (p.first.x <= q.first.x && q.first.x <= p.second.x) &&
-               (q.first.y <= p.first.y && p.first.y <= q.second.y);
+        int64_t x = q.start.x;
+        int64_t y = p.start.y;
+        return (std::min(p.start.x,p.end.x) < x && std::max(p.start.x,p.end.x) > x) && 
+               (std::min(q.start.y,q.end.y) < y && std::max(q.start.y,q.end.y) > y);
     }
     return false;
+}
+
+class Rectangle {
+public:
+    Point a;
+    Point b;
+    int64_t area;
+    bool isInside(const Point& c) const {
+        return c.x > std::min(a.x, b.x) && c.x < std::max(a.x, b.x) &&
+               c.y > std::min(a.y, b.y) && c.y < std::max(a.y, b.y);
+    }
+};
+
+bool insidePolygon(const Point& p, const std::vector<Point>& polygon, const Rectangle& bbox) {
+    // Ray-casting algorithm to determine if point is inside polygon
+    int64_t intersections = 0;
+    Segment ray = {p, {bbox.b.x+1, p.y}}; // horizontal ray to the right
+    for(size_t i = 1; i < polygon.size(); ++i) {
+        Segment edge = {polygon[i-1], polygon[i]};
+        if(intersects(ray, edge)) {
+            intersections++;
+        }
+    }
+    return (intersections % 2) == 1; // odd = inside, even = outside
 }
 
 // Split function
@@ -79,15 +99,15 @@ bool isNumeric(const std::string& s) {
 }
 
 int main() {
-    std::string file_name = "test_input.txt";
-    // std::string file_name = "input.txt";
+    // std::string file_name = "test_input.txt";
+    std::string file_name = "input.txt";
     std::ifstream file(file_name);
     if (!file.is_open()) {
         std::cerr << "Could not open file: '" << file_name << "'" << std::endl;
         return 1;
     }
 
-    std::vector<Coords> coords;
+    std::vector<Point> coords;
     std::string line;
     while (std::getline(file, line)) {
         if(line.empty()) continue;
@@ -101,21 +121,14 @@ int main() {
     // add back the first coordinate to close the loop
     coords.push_back(coords.front());
 
-    // compute polygon area using triangle formula
-    int64_t polygon_area = 0;
-    for(size_t i = 1; i < coords.size(); ++i) {
-        polygon_area += (coords[i-1].x * coords[i].y) - (coords[i].x * coords[i-1].y);
-    }
-    polygon_area = polygon_area / 2;
-    if(polygon_area < 0) {
-        // make polygon counter-clockwise oriented
-        std::reverse(coords.begin(), coords.end());
-        polygon_area = -polygon_area;
-    }
-
     // compute area of every possible rectangle from coords
     std::vector<Rectangle> rectangles;
+    Rectangle bbox = {coords.front(), coords.front(), 0};
     for(size_t i = 0; i < coords.size(); ++i) {
+        bbox.a.x = std::min(bbox.a.x, coords[i].x);
+        bbox.a.y = std::min(bbox.a.y, coords[i].y);
+        bbox.b.x = std::max(bbox.b.x, coords[i].x);
+        bbox.b.y = std::max(bbox.b.y, coords[i].y);
         for(size_t j = i + 1; j < coords.size(); ++j) {
             rectangles.push_back({coords[i], coords[j],
                 (std::abs((coords[j].x - coords[i].x))+1) *
@@ -133,32 +146,39 @@ int main() {
     // in descending area order, find first rectangle that contains no other coords in interior
     // and has no line between other coords that intersects it
     for(const auto& rect : rectangles) {
-        if(rect.area >= polygon_area) continue;
+        // check lines from the polygon cross the box
         bool crosses_rectangle = false;
-        // lower right to upper right
-        std::pair<Coords, Coords> p = {{std::max(rect.a.x, rect.b.x), std::min(rect.a.y, rect.b.y)},
-                                       {std::max(rect.a.x, rect.b.x), std::max(rect.a.y, rect.b.y)}};
-        // upper right to upper left
-        std::pair<Coords, Coords> q = {{std::max(rect.a.x, rect.b.x), std::max(rect.a.y, rect.b.y)},
-                                       {std::min(rect.a.x, rect.b.x), std::max(rect.a.y, rect.b.y)}};
-        // upper left to lower left
-        std::pair<Coords, Coords> r = {{std::min(rect.a.x, rect.b.x), std::max(rect.a.y, rect.b.y)},
-                                       {std::min(rect.a.x, rect.b.x), std::min(rect.a.y, rect.b.y)}};
-        // lower left to lower right
-        std::pair<Coords, Coords> s = {{std::min(rect.a.x, rect.b.x), std::min(rect.a.y, rect.b.y)},
-                                       {std::max(rect.a.x, rect.b.x), std::min(rect.a.y, rect.b.y)}};
+        Point lr = {std::max(rect.a.x, rect.b.x), std::min(rect.a.y, rect.b.y)};
+        Point ur = {std::max(rect.a.x, rect.b.x), std::max(rect.a.y, rect.b.y)};
+        Point ul = {std::min(rect.a.x, rect.b.x), std::max(rect.a.y, rect.b.y)};
+        Point ll = {std::min(rect.a.x, rect.b.x), std::min(rect.a.y, rect.b.y)};
+        Segment p = {lr, ur};
+        Segment q = {ur, ul};
+        Segment r = {ul, ll};
+        Segment s = {ll, lr};
         for(size_t i = 1; i<coords.size(); ++i) {
-            std::pair<Coords, Coords> v = {coords[i-1], coords[i]};
-            if(crosses(p,v) || crosses(q,v) || crosses(r,v) || crosses(s,v)) {
+            Segment v = {coords[i-1], coords[i]};
+            if(rect.isInside(v.start) || rect.isInside(v.end)) {
+                crosses_rectangle = true;
+                break;
+            }
+            if(intersects(p,v) || intersects(q,v) || intersects(r,v) || intersects(s,v)) {
                 crosses_rectangle = true;
                 break;
             }
         }
-        if(!crosses_rectangle) {
-            std::cout << "Part B: " << rect.area << std::endl;
-            // 3087332664 too high
-            break;
-        }
+        if(crosses_rectangle) continue;
+
+        // check all corners and center is inside polygon
+        if(!insidePolygon(lr, coords, bbox)) continue;
+        if(!insidePolygon(ur, coords, bbox)) continue;
+        if(!insidePolygon(ul, coords, bbox)) continue;
+        if(!insidePolygon(ll, coords, bbox)) continue;
+        Point center = {(rect.a.x + rect.b.x) / 2, (rect.a.y + rect.b.y) / 2};
+        if(!insidePolygon(center, coords, bbox)) continue;
+        
+        std::cout << "Part B: " << rect.area << std::endl;
+        break;
     }
     return 0;
 }
